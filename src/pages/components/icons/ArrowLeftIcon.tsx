1ce6ffb3e7dd2fe44a23a29d2beed9eabdef4d04
Damian <practicanteing2@smartinfo.com.co>
2024-02-02 07:31:54 -0500

    refactor: change attrs html for jsx version

--- src/pages/components/icons/ArrowLeftIcon.tsx
+++ src/pages/components/icons/ArrowLeftIcon.tsx
@@ -10,22 +10,20 @@ export default function ArrowLeftIcon() {
       xmlns="http://www.w3.org/2000/svg"
       stroke="#ffffff"
     >
-      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-      <g
-        id="SVGRepo_tracerCarrier"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      ></g>
+      <g id="SVGRepo_bgCarrier" strokeWidth={0} />
+      <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" />
       <g id="SVGRepo_iconCarrier">
-        {" "}
+        {"{"}" "{"}"}
         <path
           d="M5 12H19M5 12L11 6M5 12L11 18"
           stroke="#ffffff"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        ></path>{" "}
+          strokeWidth={2}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        {"{"}" "{"}"}
       </g>
     </svg>
+
   );
 }
