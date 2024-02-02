b4119fbdadac6bf0f7963ea4eb6e068da862be92
Damian <practicanteing2@smartinfo.com.co>
2024-02-02 07:44:29 -0500

    fix: error in head tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import { Metadata } from 'next'
-export const metadata: Metadata = {
-  title: 'Tharot',
-  description: 'Tharot',
-  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
-};
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Tharot</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
