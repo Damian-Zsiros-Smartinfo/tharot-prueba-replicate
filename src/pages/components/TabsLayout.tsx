64a3f8c1212fbabefe911130b08646a39c47b3ba
Damian <damjeronzalez23@gmail.com>
2024-02-02 23:24:23 -0500

    fix: error in styles

--- src/pages/components/TabsLayout.tsx
+++ src/pages/components/TabsLayout.tsx
@@ -37,17 +37,17 @@ export default function TabsLayout({
     <section className="w-full min-h-[94vh] bg-gray-500 py-12  grid place-items-center">
       <section className="max-w-[90%] overflow-auto w-full h-full  bg-white rounded-lg ">
         <header className="h-[60px]  bg-gray-50 ">
-          <ul className="flex gap-1 h-full items-center overflow-x-scroll overflow-y-hidden">
+          <ul className="flex gap-1 items-center overflow-x-auto overflow-y-hidden">
             {tabs.map((tab, i) => (
               <li
-                className={` rounded  transition  min-w-30 w-fit h-full p-2  ${
+                className={`border-x rounded  transition  min-w-30 w-fit h-full p-2  ${
                   i == activeIndex
-                    ? "bg-zinc-50 opacity-80 hover:opacity-100 text-gray-800 "
+                    ? "bg-white opacity-80 hover:opacity-100 text-gray-800  "
                     : "bg-gray-200 text-gray-400 hover:bg-gray-100 hover:text-gray-800"
                 }`}
                 key={i}
               >
-                <article className="cursor-pointer flex justify-between w-full transition hover:[&>svg]:scale-110  items-center [&>svg]:w-4 [&>svg]:transition [&>svg]:opacity-70 hover:[&>svg]:opacity-100">
+                <article className="cursor-pointer flex justify-between gap-2 w-full transition hover:[&>svg]:scale-110  items-center [&>svg]:w-4 [&>svg]:transition [&>svg]:opacity-70 hover:[&>svg]:opacity-100">
                   <button type="button" onClick={() => onClick(i)}>
                     <span>{tab.text}</span>
                   </button>
