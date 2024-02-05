53005519ffc09d11cfb27b192aa6a482e75a221d
Damian Zsiros Dev <69727544+Damian-Zsiros-Prog@users.noreply.github.com>
2024-02-05 09:27:15 -0500

    refactor: delete comments unnecesary

--- src/utils/getHourSpanish.ts
+++ src/utils/getHourSpanish.ts
@@ -6,7 +6,6 @@ export function getHourSpanish() {
   let minutos = ahora.getMinutes();
   const ampm = horas >= 12 ? "PM" : "AM";
 
-  // Convertir a formato de 12 horas
   horas = horas % 12 || 12;
 
   const minutesSpanish = minutos < 10 ? "0" + minutos : minutos;
