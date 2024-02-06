45a168b30ce8b6c34fb086059548d96392ce1099
Damian Zsiros Dev <69727544+Damian-Zsiros-Prog@users.noreply.github.com>
2024-02-06 10:14:04 -0500

    feat: add changes

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -6,24 +6,21 @@ interface Message {
   text: string;
 }
 
-export default function Chat() {
-  const socket = useRef(io("http://localhost:4000"));
-  const [NameActor, setNameActor] = useState<string>("");
-  const [NameObtained, setNameObtained] = useState(false);
-  const [Messages, setMessages] = useState<Message[]>([]);
+const socket = io("http://localhost:4000");
+
+function Chat({
+  NameActor,
+  Messages,
+  setMessages
+}: {
+  NameActor: string;
+  setMessages: React.Dispatch<React.SetStateAction<Message[]>>;
+  Messages: Message[];
+}) {
   const [Message, setMessage] = useState("");
 
-  useEffect(() => {
-    socket.current.on("server:loadmessages", (data: any) => {
-      setMessages(data as Message[]);
-    });
-    if (!NameObtained) {
-      setNameActor(prompt("Ingresa tu nombre para acceder al chat:") || "");
-      setNameObtained(true);
-    }
-  }, [NameObtained]);
   const onClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    socket.current.emit("server:addMessage", {
+    socket.emit("server:addMessage", {
       id: crypto.randomUUID(),
       text: Message,
       actor: NameActor
@@ -70,5 +67,65 @@ export default function Chat() {
         </footer>
       </section>
     </main>
+  );
+}
+export default function ChatPage() {
+  const [NameActor, setNameActor] = useState<string>("");
+  const [NameObtained, setNameObtained] = useState(false);
+  const [hasChange, sethasChange] = useState(false);
+  const [Messages, setMessages] = useState<Message[]>([]);
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNameActor(e.target.value);
+  };
+  const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (NameActor == "") return;
+    setNameObtained(true);
+    sethasChange(true);
+  };
+  useEffect(() => {
+    if (hasChange) {
+      console.log("dsadsa");
+      socket.on("messages", (data: any) => {
+        console.log(data);
+        setMessages(data as Message[]);
+      });
+      sethasChange(false);
+    }
+  }, [hasChange]);
+  return (
+    <>
+      {!NameObtained ? (
+        <>
+          <main className="grid place-items-center gap-4 w-full min-h-[100vh]">
+            <section className="w-full max-w-[500px] text-center flex flex-col gap-4">
+              <header className="bg-blue-500 text-white w-full py-4">
+                <h1 className="text-4xl font-bold">Ingreso al Chat</h1>
+              </header>
+              <main>
+                <input
+                  onChange={(e) => onChange(e)}
+                  type="text"
+                  className="w-full border rounded outline-none p-4 mb-4"
+                  placeholder="Ingresa tu nombre para entrar al chat"
+                />
+                <button
+                  type="button"
+                  onClick={(e) => onClick(e)}
+                  className="bg-blue-700 p-3 text-white rounded"
+                >
+                  Entrar al chat
+                </button>
+              </main>
+            </section>
+          </main>
+        </>
+      ) : (
+        <Chat
+          Messages={Messages}
+          setMessages={setMessages}
+          NameActor={NameActor}
+        />
+      )}
+    </>
   );
 }
