f38a895d1ea6ebf4644660d4103b8f13b644864d
Damian-Zsiros-Smartinfo <practicanteing2@smartinfo.com.co>
2024-02-12 16:42:24 -0500

    fix: change time ago for moment js

--- src/utils/timeAgo.ts
+++ src/utils/timeAgo.ts
@@ -1,33 +1,4 @@
-export function timeAgo(postgresTimestamp: string): string {
-  const currentDate = new Date();
-  const timestampDate = new Date(postgresTimestamp);
-
-  const timeDifferenceInSeconds = Math.floor((currentDate.getTime() - timestampDate.getTime()) / 1000);
-
-  const intervals = {
-      año: 31536000,
-      mes: 2592000,
-      día: 86400,
-      hora: 3600,
-      minuto: 60,
-      segundo: 1,
-  };
-
-  let elapsedTime = 0;
-  let unit = "";
-
-  for (const [intervalUnit, secondsInInterval] of Object.entries(intervals)) {
-      elapsedTime = Math.floor(timeDifferenceInSeconds / secondsInInterval);
-
-      if (elapsedTime >= 1) {
-          unit = intervalUnit;
-          break;
-      }
-  }
-
-  if (elapsedTime > 1) {
-      unit += "s";
-  }
-
-  return `${elapsedTime} ${unit} ago`;
+import moment from "moment";
+export function timeAgo(dateString: string): string {
+  return moment(dateString).fromNow(true);
 }
