cb0c993be8b1303b239726ab5fbe286f06611ee9
Damian-Zsiros-Smartinfo <practicanteing2@smartinfo.com.co>
2024-02-12 05:46:17 -0500

    feat: add part of functionality for receive messages images of backend

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,44 +1,45 @@
-import { constants } from "../../../constants";
-import React, { useEffect, useRef, useState } from "react";
-import { Socket, io } from "socket.io-client";
-import JoinPersonAtChatForm from "../components/JoinPersonAtChatForm";
-import { Message } from "@/types/Message";
-import { Chat } from "../components/Chat";
+import { constants } from '../../../constants'
+import React, { useEffect, useRef, useState } from 'react'
+import { Socket, io } from 'socket.io-client'
+import JoinPersonAtChatForm from '../components/JoinPersonAtChatForm'
+import { Message } from '@/types/Message'
+import { Chat } from '../components/Chat'
 
-let socket = io(constants.API_URL);
+let socket = io(constants.API_URL)
 
 export default function ChatPage() {
-  const [NameActor, setNameActor] = useState<string>("");
-  const [NameObtained, setNameObtained] = useState(false);
-  const [hasChange, sethasChange] = useState(false);
-  const [Messages, setMessages] = useState<Message[]>([]);
-  const chatContainerRef = useRef<HTMLElement>(null);
+  const [NameActor, setNameActor] = useState<string>('')
+  const [NameObtained, setNameObtained] = useState(false)
+  const [hasChange, sethasChange] = useState(false)
+  const [Messages, setMessages] = useState<Message[]>([])
+  const chatContainerRef = useRef<HTMLElement>(null)
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setNameActor(e.target.value);
-  };
+    setNameActor(e.target.value)
+  }
 
   const getMessages = () => {
     fetch(`${constants.API_URL}/messages`).then((res: Response) => {
       res.json().then((data) => {
-        const { messages } = data;
-        setMessages(messages);
-      });
-    });
-  };
+        const { messages } = data
+        console.log(messages)
+        setMessages(messages)
+      })
+    })
+  }
 
   useEffect(() => {
-    socket.on("server:loadmessages", (data) => {
-      setMessages(data);
-    });
-  }, []);
+    socket.on('server:loadmessages', (data) => {
+      setMessages(data)
+    })
+  }, [])
 
   const onClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
-    if (NameActor == "") return;
-    await getMessages();
-    setNameObtained(true);
-    sethasChange(true);
-  };
+    if (NameActor == '') return
+    await getMessages()
+    setNameObtained(true)
+    sethasChange(true)
+  }
 
   return (
     <>
@@ -54,5 +55,5 @@ export default function ChatPage() {
         />
       )}
     </>
-  );
+  )
 }
--- src/pages/components/Message.tsx
+++ src/pages/components/Message.tsx
@@ -1,50 +1,57 @@
-import { hasDifferenceOfDateMore3Min } from "@/utils/differenceOfDateMore3Min";
-import React from "react";
-import EditIcon from "./icons/EditIcon";
-import { timeAgo } from "@/utils/timeAgo";
-import { Image, Message } from "@/types/Message";
+import { hasDifferenceOfDateMore3Min } from '@/utils/differenceOfDateMore3Min'
+import React from 'react'
+import EditIcon from './icons/EditIcon'
+import { timeAgo } from '@/utils/timeAgo'
+import { Image, Message } from '@/types/Message'
+import ImageComponent from 'next/image'
 
 interface Props {
-  message: Message;
-  NameActor: string;
-  onClickDeleteMessage: (
-    e: React.MouseEvent<SVGSVGElement, MouseEvent>
-  ) => void;
-  images?: Image[] | {
-    file: {
-      name: string;
-    };
-    arrayBuffer: Buffer;
-  }[]
+  message: Message
+  NameActor: string
+  onClickDeleteMessage: (e: React.MouseEvent<SVGSVGElement, MouseEvent>) => void
+  images?:
+    | Image[]
+    | {
+        file: {
+          name: string
+        }
+        arrayBuffer: Buffer
+      }[]
 }
 
 export default function MessageComponent({
   message,
   NameActor,
   onClickDeleteMessage,
-  images
+  images,
 }: Props) {
   return (
     <article
       key={message.id}
-      className={`border p-2 rounded max-w-[70%] w-full flex gap-5 justify-between scale-100  transition ${message.actor != NameActor ? "mr-auto" : "ml-auto"
-        }`}
-    >
-      <div className="min-w-[80%]">
+      className={`border p-2 rounded max-w-[70%] w-full flex gap-5 justify-between scale-100  transition ${
+        message.actor != NameActor ? 'mr-auto' : 'ml-auto'
+      }`}>
+      <div className='min-w-[80%]'>
         <div>
-          <b className="text-[#3265b6] text-md opacity-90">
-            {message.actor == NameActor ? "Yo" : message.actor}
+          <b className='text-[#3265b6] text-md opacity-90'>
+            {message.actor == NameActor ? 'Yo' : message.actor}
           </b>
-          <p className="text-lg transition">{message.text}</p>
-          <b className="text-xs w-[100%] text-right opacity-70">
-            {timeAgo(new Date(Date.parse(message.created_at || "")))}
+          <p className='text-lg transition'>{message.text}</p>
+          <b className='text-xs w-[100%] text-right opacity-70'>
+            {timeAgo(new Date(Date.parse(message.created_at || '')))}
           </b>
         </div>
-        <div className="flex gap-4 p-4 flex-wrap justify-center">
-          {message.images?.map(image => {
+        <div className='flex gap-4 p-4 flex-wrap justify-center'>
+          {message.images?.map((image) => {
             return (
               <>
-                <img className="max-w-12" src={image.link_image} />
+                <ImageComponent
+                  alt={message.actor}
+                  width={40}
+                  height={40}
+                  className='max-w-12'
+                  src={image.link_image}
+                />
               </>
             )
           })}
@@ -52,8 +59,8 @@ export default function MessageComponent({
       </div>
 
       {!hasDifferenceOfDateMore3Min(
-        new Date(Date.parse(message.created_at || ""))
+        new Date(Date.parse(message.created_at || ''))
       ) && <EditIcon onClick={onClickDeleteMessage} />}
     </article>
-  );
+  )
 }
